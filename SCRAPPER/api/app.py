04a4f0059c0629ca7be2dcb95b481522e257b604
Icey-Python <65103243+Icey-Python@users.noreply.github.com>
2023-09-13 08:55:09 +0000

    fixed CORS

--- SCRAPPER/api/app.py
+++ SCRAPPER/api/app.py
@@ -1,23 +1,26 @@
 from flask import Flask
-from flask_cors import CORS # import CORS 
+from flask_cors import CORS, cross_origin  # import CORS
 
 import json
 from os import path
 
 app = Flask(__name__)
 
-CORS(app) # enable CORS for whole app
+CORS(app)  # enable CORS for whole app
 
-@app.route('/') 
+
+@app.route('/')
 def hello():
   return "Hello"
 
+
 @app.route("/news")
-@cross_origin() # enable CORS for this route
+@cross_origin()  # enable CORS for this route
 def give_feed():
   with open('static/content.json', 'r') as file:
     data = json.load(file)
   return data
+
 
 if __name__ == '__main__':
   app.run()
