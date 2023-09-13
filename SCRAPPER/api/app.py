71918d2683ed3fa04d674b41b7f1784b4a9b4003
Icey-Python <65103243+Icey-Python@users.noreply.github.com>
2023-09-13 10:28:27 +0000

    Handle image data from server to frontend

--- SCRAPPER/api/app.py
+++ SCRAPPER/api/app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask,request, Response
 from flask_cors import CORS, cross_origin  # import CORS
-
+import requests
 import json
-from os import path
 
 app = Flask(__name__)
 
@@ -13,6 +12,19 @@ CORS(app)  # enable CORS for whole app
 def hello():
   return "Hello"
 
+
+@app.route('/proxy-image') 
+@cross_origin()
+def proxy_image():
+  url = request.args.get('url')
+  response = requests.get(url)
+  image_data = response.content
+  response_headers = {
+    'Content-Type': response.headers['Content-Type'],
+    'Access-Control-Allow-Origin' : '*' 
+  }
+
+  return Response(image_data, headers=response_headers)
 
 @app.route("/news")
 @cross_origin()  # enable CORS for this route
